e3961f3f3def8fa955d882a7e65a31369017e0de
kirill petrov <fantaskiras@gmail.com>
2022-03-31 20:40:13 +0300

    TodoApp, added formAddTodo

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TodoApp = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    setTasks(
+      [
+        // ...tasks,
+        // {
+        //   id: tasks.length + 1,
+        //   task: e.target.value
+        // }
+      ]
+    );
+  };
   return (
     <div>
-      lkk
+      <form onSubmit={handleSubmit}>
+        <input name='add-todo' type="text" placeholder="Новая задача" />
+        <button>Сохранить</button>
+      </form>
+
+      <input name='filter' type="text" placeholder="Поиск" />
+
+      {
+        tasks.length ?
+          <p>todoList</p> :
+          <p>List empty</p>
+      }
+      <button>Удалить</button>
     </div>
   );
 };
